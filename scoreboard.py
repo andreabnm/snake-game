20c1ed5f3947c7cbdf77e6d4d466909042c7c943
Andrea Bonomi <andreabnm@gmail.com>
2023-09-11 23:34:49 +0200

    Save high score

--- scoreboard.py
+++ scoreboard.py
@@ -3,12 +3,13 @@ from turtle import Turtle
 ALIGNMENT = 'center'
 FONT = ('Courier', 24, 'normal')
 
-
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
+        self.read_high_score()
         self.color('white')
         self.hideturtle()
         self.penup()
@@ -21,10 +22,25 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write_high_score()
+        self.score = 0
+        self.update_scoreboard()
+
+    def read_high_score(self):
+        with open('data.txt', 'r') as file:
+            self.high_score = int(file.read())
+
+    def write_high_score(self):
+        with open('data.txt', 'w') as file:
+            file.write(str(self.high_score))
 
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
+
+
+
 
